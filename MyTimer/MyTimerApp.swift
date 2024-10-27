5faae75b14e313bc782b9d8056402c100b13dcb8
Oleg Lesnoy <oleglv98@gmail.com>
2024-10-27 13:17:43 +0100

    create app

---
+++ MyTimer/MyTimerApp.swift
@@ -0,0 +1,17 @@
+//
+//  MyTimerApp.swift
+//  MyTimer
+//
+//  Created by Oleg Lesnoy on 27.10.2024.
+//
+
+import SwiftUI
+
+@main
+struct MyTimerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
